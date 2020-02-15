e0d148018911d22905e6e8bcaf203cc7f9b5a267
jeromesigg <jerome.sigg+newsletter@gmail.com>
2020-02-15 14:44:09 +0100

    Update geocoder.php

--- config/geocoder.php
+++ config/geocoder.php
@@ -5,7 +5,7 @@ return [
     /*
      * The api key used when sending Geocoding requests to Google.
      */
-    'key' => env('GOOGLE_MAPS_GEOCODING_API_KEY', 'AIzaSyANAmxiZYaDqNi7q5xxC6RicESrCmQFutw'),
+    'key' => env('GOOGLE_MAPS_GEOCODING_API_KEY'),
 
     /*
      * The language param used to set response translations for textual data.
