b6f676607bd37fb9bf8989ce2194b97e73172ed2
jeromesigg <jerome.sigg+newsletter@gmail.com>
2023-05-15 20:05:26 +0200

    Update edit.blade.php

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -24,6 +24,11 @@
                     {!! Form::label('username', 'Name:') !!}
                     {!! Form::text('username', null, ['class' => 'form-control']) !!}
                 </div>
+                
+                <div class="form-group">
+                    {!! Form::label('email', 'E-Mail:') !!}
+                    {!! Form::email('email', null, ['class' => 'form-control']) !!}
+                </div>
 
                 <div class="form-group">
                 {!! Form::label('role_id', 'Rolle:') !!}
