190ea061041fe27630a5a37f395eaf4ea1669a4f
jerome.sigg <jerome.sigg+newsletter@gmail.com>
2024-06-17 21:22:20 +0200

    Letzte Forms für Flowbite angepasst

--- resources/views/admin/helps/index.blade.php
+++ resources/views/admin/helps/index.blade.php
@@ -20,15 +20,15 @@
                     <p>Artikel Erfassen:</p>
                     {!! Form::open(['method' => 'POST', 'action'=>'AdminHelpController@store']) !!}
                     <div class="form-group">
-                        {!! Form::label('title', 'Titel:') !!}
-                        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                        {!! Form::label('title', 'Titel:', ['class' =>'block mb-2 text-sm font-medium text-gray-900 dark:text-white']) !!}
+                        {!! Form::text('title', null, ['class' => 'mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500', 'required']) !!}
                     </div>
                     <div class="form-group">
-                        {!! Form::label('content', 'Inhalt:') !!}
-                        {!! Form::textarea('content', null, ['class' => 'form-control','required' ,'rows' => 10]) !!}
+                        {!! Form::label('content', 'Inhalt:', ['class' =>'block mb-2 text-sm font-medium text-gray-900 dark:text-white']) !!}
+                        {!! Form::textarea('content', null, ['class' => 'mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500', 'required', 'rows' => 10]) !!}
                     </div>
                     <div class="form-group">
-                        {!! Form::submit('Artikel Erfassen', ['class' => 'btn btn-primary'])!!}
+                        {!! Form::submit('Artikel Erfassen', ['class' => 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'])!!}
                     </div>
                     {!! Form::close()!!}
                 </div>
